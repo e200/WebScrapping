e50c7c813e0b81e7ff15c8fe9334af220a02ca14
Eleandro Duzentos <eleandro@inbox.ru>
2019-01-26 12:42:39 +0100

    Required cheerio and puppeteer to routes They will be moved later to another file

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
+const cheerio = require('cheerio')
+const client = require('puppeteer')
+
+const { URL } = require('./config/dataSource')
+
 module.exports = (app) => {
   return {
     register: () => {
       app.get('/', (req, res) => {
+        browser = client.launch()
+
+        //
+
         res.send(true)
       })
     }
