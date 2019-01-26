1a3cd7f12be5c3ec3229df97b3c6f554b1b5f8cf
Eleandro Duzentos <eleandro@inbox.ru>
2019-01-26 12:49:59 +0100

    Headless browser now loads the page content

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
-const cheerio = require('cheerio')
-const client = require('puppeteer')
+const cheerio   = require('cheerio')
+const puppeteer = require('puppeteer')
 
 const { URL } = require('./config/dataSource')
 
 module.exports = (app) => {
   return {
     register: () => {
-      app.get('/', (req, res) => {
-        browser = client.launch()
+      app.get('/', async (req, res) => {
+        const browser = await puppeteer.launch()
+        const page    = await browser.newPage()
 
-        //
+        await page.goto(URL)
+        const html = await page.content()
+
+        // cheerio.
 
         res.send(true)
       })
