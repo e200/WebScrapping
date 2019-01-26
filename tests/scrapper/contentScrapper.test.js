2b971be9d1a090742749420dcfc889b73985e938
Eleandro Duzentos <eleandro@inbox.ru>
2019-01-26 22:15:08 +0100

    Updated

--- tests/scrapper/contentScrapper.test.js
+++ tests/scrapper/contentScrapper.test.js
@@ -2,4 +2,17 @@ test('Content scrapper scrap', async () => {
   const scrapper = require('../../src/scrapper/contentScrapper')
 
   expect(scrapper).toHaveProperty('scrap')
+
+  const data = await scrapper.scrap('<span class="data">$ 450</span>', '.data')
+
+  expect(typeof data).toBe('object')
+  expect(data.length).toBe(1)
+
+  const firstDataObject = data[0]
+
+  expect(firstDataObject).toHaveProperty('currencySymbol')
+  expect(firstDataObject).toHaveProperty('currencyValue')
+
+  expect(firstDataObject['currencySymbol']).toBe('$')
+  expect(firstDataObject['currencyValue']).toBe('450')
 })
