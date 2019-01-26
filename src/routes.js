08356c7dc7d799d03f63c58d4baab6eacd5a1260
Eleandro Duzentos <eleandro@inbox.ru>
2019-01-26 13:51:59 +0100

    Loading scrapper and its dependencies

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
-const cheerio   = require('cheerio')
-const request = require('request-promise')
+const loader          = require('./scrapper/loader')
+const contentScrapper = require('./scrapper/contentScrapper')
+const scrapper        = require('./scrapper')(loader, contentScrapper)
 
 const { URL } = require('./config/dataSource')
 
@@ -7,7 +8,7 @@ module.exports = (app) => {
   return {
     register: () => {
       app.get('/', async (req, res) => {
-        const data = await scrapper.resolve(URL, '.header-content .row .col-xs-12')
+        const scrap = await scrapper.resolve(URL, '.header-content .row .col-xs-12')
 
         // const html = await request.get(URL)
 
@@ -15,7 +16,7 @@ module.exports = (app) => {
 
         // const text = $('.header-content .row .col-xs-12').text()
 
-        res.send(text)
+        res.send(scrap)
       })
     }
   }
